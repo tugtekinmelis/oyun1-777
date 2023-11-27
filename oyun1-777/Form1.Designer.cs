99f33bf17d0e0bd4903eca24229874c8308aabef
tugtekinmelis <145451246+tugtekinmelis@users.noreply.github.com>
2023-11-27 11:50:43 +0300

    Add files via upload

---
+++ oyun1-777/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace oyun1_777
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnKaydet = new System.Windows.Forms.Button();
+            this.btnGoster = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtAd = new System.Windows.Forms.TextBox();
+            this.txtSoyad = new System.Windows.Forms.TextBox();
+            this.txtPosta = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnKaydet
+            // 
+            this.btnKaydet.Location = new System.Drawing.Point(63, 183);
+            this.btnKaydet.Name = "btnKaydet";
+            this.btnKaydet.Size = new System.Drawing.Size(146, 48);
+            this.btnKaydet.TabIndex = 0;
+            this.btnKaydet.Text = "oyuncu kaydet";
+            this.btnKaydet.UseVisualStyleBackColor = true;
+            this.btnKaydet.Click += new System.EventHandler(this.btnKaydet_Click);
+            // 
+            // btnGoster
+            // 
+            this.btnGoster.Location = new System.Drawing.Point(63, 260);
+            this.btnGoster.Name = "btnGoster";
+            this.btnGoster.Size = new System.Drawing.Size(146, 48);
+            this.btnGoster.TabIndex = 1;
+            this.btnGoster.Text = "oyuncuyu göster";
+            this.btnGoster.UseVisualStyleBackColor = true;
+            this.btnGoster.Click += new System.EventHandler(this.btnGoster_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(63, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Ad:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(60, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Soyad:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(60, 101);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Epostası:";
+            // 
+            // txtAd
+            // 
+            this.txtAd.Location = new System.Drawing.Point(119, 17);
+            this.txtAd.Name = "txtAd";
+            this.txtAd.Size = new System.Drawing.Size(154, 20);
+            this.txtAd.TabIndex = 5;
+            // 
+            // txtSoyad
+            // 
+            this.txtSoyad.Location = new System.Drawing.Point(119, 57);
+            this.txtSoyad.Name = "txtSoyad";
+            this.txtSoyad.Size = new System.Drawing.Size(154, 20);
+            this.txtSoyad.TabIndex = 6;
+            // 
+            // txtPosta
+            // 
+            this.txtPosta.Location = new System.Drawing.Point(119, 94);
+            this.txtPosta.Name = "txtPosta";
+            this.txtPosta.Size = new System.Drawing.Size(154, 20);
+            this.txtPosta.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtPosta);
+            this.Controls.Add(this.txtSoyad);
+            this.Controls.Add(this.txtAd);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnGoster);
+            this.Controls.Add(this.btnKaydet);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnKaydet;
+        private System.Windows.Forms.Button btnGoster;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtAd;
+        private System.Windows.Forms.TextBox txtSoyad;
+        private System.Windows.Forms.TextBox txtPosta;
+    }
+}
+
---
+++ oyun1-777/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace oyun1_777
+{
+    public partial class Form1 : Form
+    {
+        Oyuncu oyuncu1;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            Oyuncu oyuncu1 = new Oyuncu();
+
+            oyuncu1.Id = "142555485697458548";
+            oyuncu1.Ad = "Ramiz";
+            oyuncu1.Soyad = "Yurdakul";
+            oyuncu1.Posta = "yurdakul_ramiz_2000@gmail.com";
+            oyuncu1.Puan = 100;
+            oyuncu1.KayitTarih = DateTime.Now;
+            oyuncu1.Aktif = false;
+
+            MessageBox.Show("Ad:" + oyuncu1.Ad + "\nPuan:" + oyuncu1.Puan + "\nAktif:" + oyuncu1.Aktif);
+
+    
+        }
+
+        private void btnKaydet_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+
+            oyuncu1 = new Oyuncu();
+            oyuncu1.Id = random.Next(1000,10000).ToString();
+            oyuncu1.Ad = txtAd.Text;
+            oyuncu1.Soyad = txtSoyad.Text;
+            oyuncu1.Posta = txtPosta.Text;
+            oyuncu1.KayitTarih = DateTime.Now;
+            oyuncu1.Puan = 100; //isterseniz random yaparız.
+            oyuncu1.Aktif = false;
+        }
+
+        private void btnGoster_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("ID:" + oyuncu1.Id + "\nAd:" + oyuncu1.Puan + "\nKayıt:" + oyuncu1.KayitTarih);
+            //Her sınıf için bu işi yapan özel bir metot vardır. Bu metoda "kurucu metot" denir.
+        }
+    }
+}
